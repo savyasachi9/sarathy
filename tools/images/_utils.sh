48e8527e5baf20a8cfef0548dc3e45bc258a1f96
rpunia7 <rpunia7@gmail.com>
2022-02-01 21:05:08 -0800

    moved some things around for simplification

--- tools/images/_utils.sh
+++ tools/images/_utils.sh
@@ -24,6 +24,21 @@ PATH="${BIN_PATH}:${PATH}"
 wget -q -O minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-${ARCH} \
     && chmod +x minikube && mv minikube ${BIN_PATH}
 
+echo "
+[Unit]
+Description=minikube
+After=docker.service
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/utils/k8s/minikube start --driver=none --extra-config=kubeadm.ignore-preflight-errors=SystemVerification
+User=docker
+Group=docker
+
+[Install]
+WantedBy=multi-user.target
+" | tee /etc/systemd/system/minikube.service
+
 # kubectl with bash completion / https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 wget -q -O kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl" \
     && chmod +x kubectl && mv kubectl ${BIN_PATH}
@@ -54,6 +69,8 @@ wget -q -O tilt.tar.gz https://github.com/tilt-dev/tilt/releases/download/v0.23.
 wget -q -O k9s.tar.gz https://github.com/derailed/k9s/releases/download/v0.24.1/k9s_Linux_${ARCH_ALIAS}.tar.gz \
     && tar -xzf k9s.tar.gz k9s && chmod +x k9s && mv k9s ${BIN_PATH}
 
+# TODO: k8s lens
+
 ###################
 # container tools :
 BIN_PATH=/usr/local/bin/utils/docker
@@ -69,15 +86,50 @@ mkdir -p ${BIN_PATH}
 PATH="${BIN_PATH}:${PATH}"
 
 # gotty (not available for arm64)
-if [[ "${ARCH}" == 'amd64' ]]; then \
+if [[ "${ARCH}" == 'amd64' ]]; then
     wget -q -O gotty.tar.gz https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz \
-    && tar -xzf gotty.tar.gz && chmod +x gotty && mv gotty ${BIN_PATH} \
-; fi
+    && tar -xzf gotty.tar.gz && chmod +x gotty && mv gotty ${BIN_PATH};
 
-# croc TODO:
+    echo "
+[Unit]
+Description=webtty
+
+[Service]
+ExecStart=/usr/local/bin/utils/web/gotty -a 0.0.0.0 -p 9090 -w bash
+User=docker
+
+[Install]
+WantedBy=multi-user.target
+" | tee /etc/systemd/system/webtty.service
+
+    systemctl enable webtty
+fi
+
 PACKAGES_PATH=/tmp/packages/deb
 mkdir -p ${PACKAGES_PATH}
+
+# vscode in browser (code-server)
 wget -q -O ${PACKAGES_PATH}/code-server.deb https://github.com/coder/code-server/releases/download/v4.0.1/code-server_4.0.1_${ARCH}.deb
+dpkg -i ${PACKAGES_PATH}/code-server.deb
+
+echo "
+[Unit]
+Description=vscode
+
+[Service]
+ExecStart=code-server --auth none --bind-addr 0.0.0.0:9091
+User=docker
+
+[Install]
+WantedBy=multi-user.target
+" | tee /etc/systemd/system/vscode.service
+systemctl enable vscode
+
+# TODO: IPFS support
+# TODO: croc for sending files over web
+
+# TODO: SMTP support
+# TODO: golang support for msgbox & sarathy pkgs
 
 # prepend to file, so PATH is always up top
 printf "export PATH=$PATH\n" > /tmp/path
