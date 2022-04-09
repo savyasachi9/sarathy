988a960f91aec29c47195722d18ecdc964c73c25
rpunia7 <rpunia7@gmail.com>
2022-04-09 14:11:07 -0700

    MVP: added app.sh wrapper for task cmd

---
+++ apps/app.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+###
+# This func is a wrapper for 'task' cmd to make things simpler for the end user like
+# task -t FILE_PATH/taskfile.yaml cmd -> app name cmd
+# eg: app mysql test
+###
+app(){
+  APPS_HOME=/src/apps
+  if [[ -d '/src/user/apps' ]]; then
+    APPS_HOME=/src/user/apps
+  fi
+
+  APP_NAME=$1
+  APP_CMD=$2
+  if [ -z ${EDITOR+x} ]; then EDITOR=code-server; fi
+  if [[ $APP_NAME == '' ]]; then echo "ERR: app taskfile name not given, exiting ..."; return 1; fi
+
+  # let's find app file
+  # TODO: check for if we've found > 1 file
+  K8S_APPS=${APPS_HOME}/k8s
+  APP_DIR=$(fd $APP_NAME $APPS_HOME)
+  APP_TASKFILE=$APP_DIR/taskfile.yaml
+  TASKFILE_LIB=${APPS_HOME}/taskfile-lib
+
+  #echo $APP_NAME $EDITOR ':' $APP_DIR $APP_TASKFILE $APP_CMD $TASKFILE_LIB
+
+  # let's pass all ARGS to task cmd except ARG1, as it's the APP_FILE
+  TASKFILE=$APP_TASKFILE TASKFILE_LIB=$TASKFILE_LIB\
+  APP_NAME=$APP_NAME APP_DIR=$APP_DIR \
+  K8S_APPS=$K8S_APPS \
+  EDITOR=$EDITOR \
+    task -t $APP_TASKFILE "${@:2}"
+}
--- apps/k8s/README.md
+++ apps/k8s/README.md
@@ -1,8 +1,8 @@
 ### Mysql deploy/delete @ k8s cluster using 'Taskfile'
 ```bash
-task -t apps/k8s/dbs/mysql.yaml deploy
-task -t apps/k8s/dbs/mysql.yaml test
-task -t apps/k8s/dbs/mysql.yaml delete
+app mysql deploy
+app mysql test
+app mysql delete
 
 # Mysql gets deployed as a NodePort Service so you can access it from sarathy like this
 # mysql -h 0.0.0.0 -u root -p root
@@ -10,9 +10,9 @@ task -t apps/k8s/dbs/mysql.yaml delete
 
 ### Redis deploy/delete @ k8s cluster using 'Taskfile'
 ```bash
-task -t apps/k8s/dbs/redis.yaml deploy
-task -t apps/k8s/dbs/redis.yaml test
-task -t apps/k8s/dbs/redis.yaml delete
+app redis deploy
+app redis test
+app redis delete
 
 # Redis gets deployed as a NodePort Service so you can access it from sarathy like this
 # redis-cli -h 0.0.0.0
